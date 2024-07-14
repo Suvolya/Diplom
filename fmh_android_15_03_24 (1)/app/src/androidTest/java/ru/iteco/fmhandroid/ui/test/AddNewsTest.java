b298b2a09b8ff7c6c0770fa4d859de360a8dbf02
Suvolya <Olga.art.gm@gmail.com>
2024-07-14 15:43:17 +0300

    Внесены изменения в POM и добавлены тесты

---
+++ fmh_android_15_03_24 (1)/app/src/androidTest/java/ru/iteco/fmhandroid/ui/test/AddNewsTest.java
@@ -0,0 +1,67 @@
+package ru.iteco.fmhandroid.ui.test;
+
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.replaceText;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
+import androidx.test.espresso.DataInteraction;
+import androidx.test.espresso.ViewInteraction;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ru.iteco.fmhandroid.R;
+import ru.iteco.fmhandroid.ui.AppActivity;
+import ru.iteco.fmhandroid.ui.page.LoginPage;
+import ru.iteco.fmhandroid.ui.page.NewsPage;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class AddNewsTest {
+
+    NewsPage newsPage = new NewsPage();
+
+    @Rule
+    public ActivityScenarioRule<AppActivity> mActivityScenarioRule =
+            new ActivityScenarioRule<>(AppActivity.class);
+
+    @Test
+    public void AddNewsTest() {
+        newsPage.clickMenuButn();
+        newsPage.selectMenuItem("Новости", "Новости");
+        newsPage.addNews("Объявление", "Объявление");
+        newsPage.testSetDateInDatePicker();
+        newsPage.testSetTimeInTimePicker();
+        newsPage.setDescriptionTextbox();
+        newsPage.clickSaveButton();
+        newsPage.setDescriptionTextBox();
+
+    }
+
+}
